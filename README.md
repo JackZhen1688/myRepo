fa0be82d59356da8ab4f80c2663a656b44d781c2
Zhen <1513191150@mil>
2019-03-21 14:46:24 -0400

    a

--- README.md
+++
@@ -1,4 +0,0 @@
-# myRepo
-This is the first test for myRepo
-Wirteing from master
-Chnage in developer
---
+++ TestProject/TestProject/src/test/java/mil/dsca/dsams/FileTest_1.java
@@ -0,0 +1,11 @@
+package mil.dsca.dsams;
+
+public class FileTest_1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello 111111");
+		
+	}
+
+}
