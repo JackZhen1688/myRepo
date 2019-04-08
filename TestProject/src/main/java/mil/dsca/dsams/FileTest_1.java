50f04a78f463ebbe31026ac2f61042f065109a9b
Zhen <1513191150@mil>
2019-04-08 10:08:29 -0400

    b

--- TestProject/src/main/java/mil/dsca/dsams/FileTest_1.java
+++ TestProject/src/main/java/mil/dsca/dsams/FileTest_1.java
@@ -9,7 +9,7 @@ public class FileTest_1 {
 		System.out.println("Hello World 333333 (mst)");
 		System.out.println("Hello World 444444 (dev)");
 		System.out.println("Hello World 555555 (tst)");
-		System.out.println("Hello World 666666 (tst)"); 
+		System.out.println("Hello World 123123 (tst)"); 
 
 
 	}
