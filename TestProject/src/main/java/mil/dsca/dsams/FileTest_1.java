5d37f86492c5ead3225c65f89e2a6041f9f17879
Zhen <1513191150@mil>
2019-04-08 09:09:28 -0400

    c

--- TestProject/src/main/java/mil/dsca/dsams/FileTest_1.java
+++ TestProject/src/main/java/mil/dsca/dsams/FileTest_1.java
@@ -9,7 +9,8 @@ public class FileTest_1 {
 		System.out.println("Hello World 333333 (mst)");
 		System.out.println("Hello World 444444 (dev)");
 		System.out.println("Hello World 555555 (tst)");
-		System.out.println("Hello World 666666 (tst)");
+		System.out.println("Hello World 123456 (tst)");
+		System.out.println("Hello World 777777 (tst)");
 
 	}
 }
